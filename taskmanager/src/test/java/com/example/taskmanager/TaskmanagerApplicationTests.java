c4341370521ec00cfc3180937dd8254322a0dfaf
Kacper Korzekwa <kackor11@gmail.com>
2026-06-18 03:06:50 +0200

    Fix CI/CD: Dodanie profilu testowego H2 i wyłączenie Redisa

--- taskmanager/src/test/java/com/example/taskmanager/TaskmanagerApplicationTests.java
+++ taskmanager/src/test/java/com/example/taskmanager/TaskmanagerApplicationTests.java
@@ -2,12 +2,15 @@ package com.example.taskmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // To wymusza użycie pliku application-test.properties!
 class TaskmanagerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Ten test po prostu weryfikuje, czy aplikacja wstaje bez błędów.
 	}
 
 }
